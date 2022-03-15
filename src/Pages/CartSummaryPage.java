30045e99e825d442ceef12c2f6f0490a11d2b502
IvaNikolov <ivanikolov86@gmail.com>
2022-03-15 17:46:03 +0100

    Cart Summary

--- src/Pages/CartSummaryPage.java
+++ src/Pages/CartSummaryPage.java
@@ -1,5 +1,21 @@
 package Pages;
 
-public class CartSummaryPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class CartSummaryPage extends BasicPage{
+
+	public CartSummaryPage(WebDriver driver, JavascriptExecutor javascriptExecutor, WebDriverWait waiter) {
+		super(driver, javascriptExecutor, waiter);
+		// TODO Auto-generated constructor stub
+	}
+public WebElement getClearAllBtn() {
+	return this.driver.findElement(By.xpath("//*[@class='cart-head']/a[2]"));
+}
+public void ClearCart() {
+	this.getClearAllBtn().click();
+}
 }
