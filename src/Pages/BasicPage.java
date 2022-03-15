7a22df713d8ed2a8641521dbf9705d5ba3c40cdc
IvaNikolov <ivanikolov86@gmail.com>
2022-03-15 17:44:46 +0100

    Basic Page

--- src/Pages/BasicPage.java
+++ src/Pages/BasicPage.java
@@ -1,5 +1,19 @@
 package Pages;
 
-public class BasicPage {
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BasicPage {
+	protected WebDriver driver;
+	protected JavascriptExecutor javascriptExecutor;
+	protected WebDriverWait waiter;
+
+	public BasicPage(WebDriver driver, JavascriptExecutor javascriptExecutor, WebDriverWait waiter) {
+
+		this.driver = driver;
+		this.javascriptExecutor = javascriptExecutor;
+		this.waiter = waiter;
+	}
 
 }
