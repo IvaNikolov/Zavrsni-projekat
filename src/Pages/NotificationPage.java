24b815b209fb5d7f835c59a2b18ce1756eb972ba
IvaNikolov <ivanikolov86@gmail.com>
2022-03-15 17:52:23 +0100

    Notification Page

--- src/Pages/NotificationPage.java
+++ src/Pages/NotificationPage.java
@@ -1,5 +1,26 @@
 package Pages;
 
-public class NotificationPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class NotificationPage extends BasicPage {
+
+	public NotificationPage(WebDriver driver, JavascriptExecutor javascriptExecutor, WebDriverWait waiter) {
+		super(driver, javascriptExecutor, waiter);
+		// TODO Auto-generated constructor stub
+	}
+public WebElement getMessage() {
+	return this.driver.findElement(By.xpath("//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]\r\n" + 
+			""));
+}
+public String getMessageText() {
+	return this.getMessage().getText();
+}
+public void waitTillMessageGone() {
+	this.waiter.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//*[contains(@class, 'system_message')][contains(@style,'display: none;')]")));
+}
 }
